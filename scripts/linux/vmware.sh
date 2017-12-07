3feda6bb6147e280e5473c4e758512b75301a6a4
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-12-07 13:07:42 +0000

    New templates to build Vagrant boxes for CentOS 6.9 and 7.4

---
+++ scripts/linux/vmware.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ ! $PACKER_BUILDER_TYPE = vmware-iso -a ! $PACKER_BUILDER_TYPE = vmware-vmx ]; then
+    exit 0
+fi
+
+if [ -f /etc/redhat-release ]; then
+    yum -y install perl fuse-libs
+    yum -y install kernel-devel-`uname -r`
+    yum -y install gcc make gcc-c++ zlib-devel openssl-devel readline-devel perl dkms fuse-utils nfs-utils
+fi
+
+# Install the VMWare Tools from a Linux ISO.
+mkdir -p /mnt/vmware
+mount -o loop /tmp/linux.iso /mnt/vmware
+tar xzf /mnt/vmware/VMwareTools-*.tar.gz -C /tmp
+umount /mnt/vmware
+rmdir /mnt/vmware
+rm -f /tmp/linux.iso
+
+# use force install option so that hgfs is installed for vagrant
+/tmp/vmware-tools-distrib/vmware-install.pl --default --force-install
+rm -fr /tmp/vmware-tools-distrib
