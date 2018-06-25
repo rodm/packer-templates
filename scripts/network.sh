c1f5a87682dbbd83a167dad8c4c96f697ceb2035
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-06-25 16:30:10 +0100

    New build agent template using Ubuntu 18.04

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-cat > /etc/network/interfaces.d/ens160 <<EOF
+. /etc/lsb-release
+
+if [ "${DISTRIB_CODENAME}" = "bionic" ]; then
+    cat > /etc/netplan/01-netcfg.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    ens160:
+      dhcp4: true
+EOF
+else
+    cat > /etc/network/interfaces.d/ens160 <<EOF
 auto ens160
 iface ens160 inet dhcp
 EOF
+fi
