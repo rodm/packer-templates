2f01c271e3de18d8a7bbc088cce929f842a882c2
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2020-07-14 13:08:26 +0100

    New vagrant template for Ubuntu 20.04

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -2,15 +2,11 @@
 
 # Setup sudo to allow no-password sudo
 if [ -f /etc/lsb-release ]; then
-    # Load up the release information
-    . /etc/lsb-release
-
-    sed -i -e 's/%admin .*/%admin ALL=(ALL) NOPASSWD:ALL/' /etc/sudoers
-    if [ "${DISTRIB_CODENAME}" = "precise" -o "${DISTRIB_CODENAME}" = "trusty" -o "${DISTRIB_CODENAME}" = "xenial" ]; then
-        sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
-    fi
-    groupadd -r admin
-    usermod -a -G admin vagrant
+    cat > /etc/sudoers.d/vagrant << EOF
+Defaults:vagrant !requiretty
+vagrant        ALL=(ALL)       NOPASSWD: ALL
+EOF
+    chmod 440 /etc/sudoers.d/vagrant
 fi
 
 # Vagrant specific
